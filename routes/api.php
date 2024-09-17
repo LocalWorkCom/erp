9e694731bf16f94c05db35c01915891e4ae08aaa
Omnia <omnia_abdelkader@saeeh.com>
2024-09-17 11:22:49 +0300

    route update

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,4 @@
 <?php
-
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ColorController;
 use App\Http\Controllers\Api\CountryController;
@@ -14,13 +13,11 @@ use App\Http\Controllers\Api\SizeController;
 use App\Http\Controllers\Api\StoreTransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\Api\StoreController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\BranchController;
 use App\Http\Controllers\Api\VendorController;
 use App\Http\Controllers\Api\ProductUnitController;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -31,19 +28,14 @@ use App\Http\Controllers\Api\ProductUnitController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
 Route::post("register", [AuthController::class, "register"]);
 Route::post("login", [AuthController::class, "login"])->name('login');
-
 Route::group(["middleware" => ["auth:api"]], function () {
-
     Route::get("profile", [AuthController::class, "profile"]);
     Route::get("logout", [AuthController::class, "logout"]);
-
-
     //Color
     Route::group(['prefix' => 'color'], function () {
         Route::get('/', [ColorController::class, 'index']);
@@ -68,78 +60,59 @@ Route::group(["middleware" => ["auth:api"]], function () {
         Route::post('/edit', [CountryController::class, 'update']);
         Route::get('/delete', [CountryController::class, 'destroy']);
     });
-
     //Opening balance
-
-
     Route::group(['prefix' => 'category'], function () {
         Route::get('/', [CategoryController::class, 'index']);
         Route::post('store', [CategoryController::class, 'store']);
         Route::post('update/{id}', [CategoryController::class, 'update']);
         Route::get('delete/{id}', [CategoryController::class, 'delete']);
     });
-
     Route::group(['prefix' => 'api_code'], function () {
         Route::get('/', [ApiCodeController::class, 'index']);
         Route::post('store', [ApiCodeController::class, 'store']);
         Route::post('update/{id}', [ApiCodeController::class, 'update']);
     });
-
-
     Route::group(['prefix' => 'product'], function () {
         Route::get('/', [ProductController::class, 'index']);
         Route::post('store', [ProductController::class, 'store']);
         Route::post('update/{id}', [ProductController::class, 'update']);
         Route::get('delete/{id}', [ProductController::class, 'delete']);
-
         Route::get('units', [ProductUnitController::class, 'index']);
         Route::post('unit/store', [ProductUnitController::class, 'store']);
         Route::post('unit/update/{id}', [ProductUnitController::class, 'update']);
         Route::get('unit/delete/{id}', [ProductUnitController::class, 'delete']);
-
         Route::get('sizes', [ProductSizeController::class, 'index']);
         Route::post('size/store', [ProductSizeController::class, 'store']);
         Route::post('size/update/{id}', [ProductSizeController::class, 'update']);
         Route::get('size/delete/{id}', [ProductSizeController::class, 'delete']);
-
         Route::get('colors', [ProductColorController::class, 'index']);
         Route::post('color/store', [ProductColorController::class, 'store']);
         Route::post('color/update/{id}', [ProductColorController::class, 'update']);
         Route::get('color/delete/{id}', [ProductColorController::class, 'delete']);
     });
-
     Route::group(['prefix' => 'unit'], function () {
         Route::get('/', [UnitController::class, 'index']);
         Route::post('store', [UnitController::class, 'store']);
         Route::post('update', [UnitController::class, 'update']);
         Route::get('delete', [UnitController::class, 'delete']);
     });
-
-
-
     Route::get('notifications', [NotificationController::class, 'index']);
     Route::post('notification/store', [NotificationController::class, 'store']);
-
     //Product
     Route::group(['prefix' => 'products'], function () {
-
         //ProductTransaction
         Route::get('productTransactions', [ProductTransactionController::class, 'index']);
         Route::get('showProductTransactions/{id}', [ProductTransactionController::class, 'show']);
         Route::post('storeProductTransactions', [ProductTransactionController::class, 'store']);
     });
-
-
     //store
     Route::group(['prefix' => 'stores'], function () {
-
         //StoreTransaction
         Route::get('storeTransactions', [StoreTransactionController::class, 'index']);
         Route::get('showStoreTransactions/{id}', [StoreTransactionController::class, 'show']);
         Route::post('addStoreTransactions', [StoreTransactionController::class, 'store']);
         Route::get('showStoreItems', [StoreTransactionController::class, 'show_products']);
         Route::get('showStoreOneItem/{id}', [StoreTransactionController::class, 'show_one_product']);
-
         //stores handling
         Route::get('storeList', [StoreController::class, 'index']);
         Route::get('showStore/{id}', [StoreController::class, 'show']);
@@ -147,16 +120,7 @@ Route::group(["middleware" => ["auth:api"]], function () {
         Route::put('updateStore/{id}', [StoreController::class, 'update']);
         Route::delete('deleteStore/{id}', [StoreController::class, 'destroy']);
     });
-
-    //  branches
-    Route::group(['prefix' => 'branches'], function () {
-        Route::get('branchList', [BranchController::class, 'index']);
-        Route::get('showBranch/{id}', [BranchController::class, 'show']);
-        Route::post('addBranch', [BranchController::class, 'store']);
-        Route::put('updateBranch/{id}', [BranchController::class, 'update']);
-        Route::delete('deleteBranch/{id}', [BranchController::class, 'destroy']);
-    });
-
+ 
 
     // vendors
     Route::group(['prefix' => 'vendors'], function () {
@@ -168,10 +132,6 @@ Route::group(["middleware" => ["auth:api"]], function () {
         Route::post('restoreVendor/{id}', [VendorController::class, 'restore']);
     });
 });
-Route::get('categories', [CategoryController::class, 'index']);
-Route::get('products', [ProductController::class, 'index']);
-Route::get('notifications', [NotificationController::class, 'index']);
-Route::get('notification/store', [NotificationController::class, 'store']);
 
 //Product
 Route::group(['prefix' => 'products'], function () {
