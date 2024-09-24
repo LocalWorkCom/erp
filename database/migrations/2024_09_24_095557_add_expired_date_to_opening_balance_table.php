444d339ada17dcfc41ce5898fdbe01461469b437
unknown <eng.noda102gmail.com>
2024-09-24 14:45:53 +0300

    add expired date to opining balance

--- database/migrations/2024_09_24_095557_add_expired_date_to_opening_balance_table.php
+++ database/migrations/2024_09_24_095557_add_expired_date_to_opening_balance_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('opening_balances', function (Blueprint $table) {
+        Schema::table('opening_balance', function (Blueprint $table) {
             $table->date('expired_date')->nullable();
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('opening_balances', function (Blueprint $table) {
+        Schema::table('opening_balance', function (Blueprint $table) {
             //
         });
     }
