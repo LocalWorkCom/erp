6e401dbb5cd545179ad5a9d5e5562267867da6a7
nouraa17 <nourmuhmd17@gmail.com>
2024-11-05 15:34:40 +0200

    penalty and delay deduction

--- app/Models/DelayDeduction.php
+++ app/Models/DelayDeduction.php
@@ -4,8 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DelayDeduction extends Model
 {
-    use HasFactory;
+    use HasFactory, softDeletes;
+
+    protected $fillable = [
+        'delay_id',
+        'deduction_amount',
+        'created_by',
+        'modified_by',
+        'deleted_by',
+
+    ];
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        'created_by',
+        'modified_by',
+        'deleted_by',
+    ];
+
+    public function delay()
+    {
+        return $this->belongsTo(Delay::class, 'delay_id')->withTrashed();
+    }
 }
