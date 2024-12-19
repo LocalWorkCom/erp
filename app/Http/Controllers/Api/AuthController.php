7d6475b7245ba8a43287d60581f169550bcd9882
KhloodIbrahim <khlood.tbgp@gmail.com>
2024-12-19 12:12:26 +0200

    response fix

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -27,7 +27,6 @@ class AuthController extends Controller
         $validator = Validator::make($request->all(), [
             "name" => "required|string",
             "email" => "required|email|unique:users",
-            'country_id' => 'nullable|exists:countries,id',
             'country_code' => 'required|string',
             "password" => "required",
             'phone' => [
@@ -150,10 +149,9 @@ class AuthController extends Controller
             ->exists();
 
         if (!$userExists) {
-            return RespondWithBadRequestWithData([
-                "phone" => [$messages["phone.exists"]]
-            ]);
+            return respondError('Validation Error.', 400, $messages["phone.exists"]);
         }
+
 
         return response()->json([
             'status' => true,
@@ -188,7 +186,7 @@ class AuthController extends Controller
         ], $messages);
 
         if ($validator->fails()) {
-            return RespondWithBadRequestWithData($validator->errors());
+            return respondError('Validation Error.', 400, $validator->errors());
         }
 
         $user = User::where('phone', $request->phone)
@@ -196,9 +194,7 @@ class AuthController extends Controller
             ->first();
 
         if (!$user) {
-            return RespondWithBadRequestWithData([
-                "phone" => [$messages["phone.exists"]]
-            ]);
+            return respondError('Validation Error.', 400, $messages["phone.exists"]);
         }
 
         // Check if the new password is the same as the old one
