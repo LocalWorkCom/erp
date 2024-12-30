0dbfa866fe13ca046491d55e3cf45907ce9c47fb
sara55mohamed <smohamedamer4@gmail.com>
2024-12-30 15:22:15 +0200

    solve issues8

--- app/Http/Controllers/Api/SliderController.php
+++ app/Http/Controllers/Api/SliderController.php
@@ -11,21 +11,37 @@ class SliderController extends Controller
 {
     protected $lang;
     public function index(Request $request){
-        $this->lang = $request->header('lang','ar');
+        $this->lang = $request->header('lang', 'ar');
         $sliders = Slider::with(['dish', 'offer'])->get();
-        foreach($sliders as $slider){
-            if($slider->dish)
-            $slider->dish->makeHidden(['name_site', 'description_site']);
+        
+        foreach ($sliders as $slider) {
+            if ($slider->dish) {
+                $slider->dish->makeHidden(['name_site', 'description_site']);
+            }
+        
             $slider->makeHidden(['name_ar', 'description_ar', 'name_en', 'description_en']);
-            if(request()->header('lang', 'ar') === 'en'){
-                $slider['name'] = $slider->name_en;
-                $slider['description'] = $slider->description_en;
-            }else{
-                $slider['name'] = $slider->name_ar;
-                $slider['description'] = $slider->description_ar;
+        
+            if ($this->lang === 'en') {
+                $slider['name'] = $slider->name_en ?? '';
+                $slider['description'] = $slider->description_en ?? '';
+            } else {
+                $slider['name'] = $slider->name_ar ?? '';
+                $slider['description'] = $slider->description_ar ?? '';
+            }
+        
+            if ($slider->offer) {
+                $slider->offer->makeHidden(['name_ar', 'description_ar', 'name_en', 'description_en']);
+        
+                if ($this->lang === 'en') {
+                    $slider->offer['name'] = $slider->offer->name_en ?? '';
+                    $slider->offer['description'] = $slider->offer->description_en ?? '';
+                } else {
+                    $slider->offer['name'] = $slider->offer->name_ar ?? '';
+                    $slider->offer['description'] = $slider->offer->description_ar ?? '';
+                }
             }
         }
-
+        
     if (!$sliders) {
         return RespondWithBadRequestData($this->lang, 2);
     }
