47d770f33b4242afed80f0192eccfab01dbc24d8
sara55mohamed <smohamedamer4@gmail.com>
2024-12-30 14:49:48 +0200

    solve issues6

--- app/Http/Controllers/Api/MostPopularController.php
+++ app/Http/Controllers/Api/MostPopularController.php
@@ -13,7 +13,16 @@ class MostPopularController extends Controller
         $popularDishes = getMostDishesOrdered(5);
         foreach($popularDishes as $popularDish){
             if($popularDish)
-            $popularDish->makeHidden(['name_site', 'description_site']);
+            //$popularDish->makeHidden(['name_site', 'description_site']);
+            $popularDish->makeHidden(['name_ar', 'description_ar', 'name_en', 'description_en']);
+            if(request()->header('lang', 'ar') === 'en'){
+                $popularDish['name'] = $popularDish->name_en;
+                $popularDish['description'] = $popularDish->description_en;
+            }else{
+                $popularDish['name'] = $popularDish->name_ar;
+                $popularDish['description'] = $popularDish->description_ar;
+            }
+
         }
 
         if (!$popularDishes) {
