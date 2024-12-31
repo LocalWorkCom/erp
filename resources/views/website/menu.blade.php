63ec8a746a79a8f851d2c54c9d9b09350f8776d8
shrookallam <shrookallam67@gmail.com>
2024-12-31 13:38:36 +0200

    solve issus10

--- resources/views/website/menu.blade.php
+++ resources/views/website/menu.blade.php
@@ -21,30 +21,25 @@
                     {{ session('success') }}
                 </div>
             @endif
-{{--
-            @if (session('error'))
-                <div class="alert alert-danger">
-                    {{ session('error') }}
-                </div>
-            @endif --}}
             <ul class="nav nav-pills mb-3 px-0 align-items-center" id="pills-tab" role="tablist">
                 @foreach ($menuCategories as $key => $menuCategory)
-                @if ($menuCategory->dish_categories)
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link {{ ($categoryId == $menuCategory->dish_categories->id || (is_null($categoryId) && $key == 0)) ? 'active' : '' }}"
-                            id="pills-{{ $menuCategory->id }}-tab" data-bs-toggle="pill"
-                            data-bs-target="#pills-{{ $menuCategory->id }}" type="button" role="tab"
-                            aria-controls="pills-{{ $menuCategory->id }}"
-                            aria-selected="{{ $key == 0 ? 'true' : 'false' }}">
-                            <div class="category-button">
-                                <img src="{{ asset($menuCategory->dish_categories->image_path ?? 'front/AlKout-Resturant/SiteAssets/images/logo-with-white-bg.png') }}"
-                                    alt="{{ $menuCategory->dish_categories->name_ar }}" />
-                                <p class="me-3 mb-0">{{ $menuCategory->dish_categories->name_ar }}</p>
-                            </div>
-                        </button>
-                    </li>
-                @endif
-            @endforeach
+                    @if ($menuCategory->dish_categories)
+                        <li class="nav-item" role="presentation">
+                            <button
+                                class="nav-link {{ $categoryId == $menuCategory->dish_categories->id || (is_null($categoryId) && $key == 0) ? 'active' : '' }}"
+                                id="pills-{{ $menuCategory->id }}-tab" data-bs-toggle="pill"
+                                data-bs-target="#pills-{{ $menuCategory->id }}" type="button" role="tab"
+                                aria-controls="pills-{{ $menuCategory->id }}"
+                                aria-selected="{{ $key == 0 ? 'true' : 'false' }}">
+                                <div class="category-button">
+                                    <img src="{{ asset($menuCategory->dish_categories->image_path ?? 'front/AlKout-Resturant/SiteAssets/images/logo-with-white-bg.png') }}"
+                                        alt="{{ $menuCategory->dish_categories->name_ar }}" />
+                                    <p class="me-3 mb-0">{{ $menuCategory->dish_categories->name_ar }}</p>
+                                </div>
+                            </button>
+                        </li>
+                    @endif
+                @endforeach
 
                 {{-- <li class="nav-item" role="presentation">
                     <form id="searchForm">
@@ -56,60 +51,64 @@
 
             <div class="d-flex justify-content-end">
                 <select class="form-select form-select-lg mb-3 w-25" aria-label=".form-select-lg example">
-                    <option selected>ترتيب حسب  @lang('header.myaddress')</option>
-                    <option value="1">الاعلى تقييم @lang('header.myaddress')</option>
-                    <option value="2">اضيف حديثا @lang('header.myaddress')</option>
+                    <option selected>ترتيب حسب @lang('header.orderby')</option>
+                    <option value="1">@lang('header.rateall')</option>
+                    <option value="2">اضيف حديثا @lang('header.newadd')</option>
                 </select>
             </div>
 
 
             <div class="tab-content pt-5" id="pills-tabContent">
                 @foreach ($menuCategories as $key => $menuCategory)
-                @if ($menuCategory->dish_categories)
-                    <div class="tab-pane fade {{ ($categoryId == $menuCategory->dish_categories->id || (is_null($categoryId) && $key == 0)) ? 'show active' : '' }}"
-                        id="pills-{{ $menuCategory->id }}" role="tabpanel"
-                        aria-labelledby="pills-{{ $menuCategory->id }}-tab">
-                        <div class="row mx-0">
-                            @foreach ($menuCategory->dish_categories->dishes as $dish)
-                                <div class="col-md-4 mb-4 dish-card {{ in_array($dish->id, $userFavorites) ? 'favorite' : '' }}"
-                                    data-dish-id="{{ $dish->id }}" data-dish-name="{{ $dish->name_ar }}" data-aos="zoom-in">
-                                    <div class="plate">
-                                        <a href="#">
-                                            <figure class="plate-img m-0">
-                                                <img src="{{ asset($dish->image ?? 'front/AlKout-Resturant/SiteAssets/images/logo-with-white-bg.png') }}"
-                                                    alt="{{ $dish->name_ar }}">
-                                            </figure>
-                                        </a>
-                                        <div class="fav">
-                                            <form action="{{ route('add.favorite') }}" method="POST" class="favorite-form">
-                                                @csrf
-                                                <input type="hidden" name="dish_id" value="{{ $dish->id }}">
-                                                <button type="submit" class="btn">
-                                                    <i class="{{ in_array($dish->id, $userFavorites) ? 'fas' : 'far' }} fa-heart"></i>
-                                                </button>
-                                            </form>
-                                        </div>
+                    @if ($menuCategory->dish_categories)
+                        <div class="tab-pane fade {{ $categoryId == $menuCategory->dish_categories->id || (is_null($categoryId) && $key == 0) ? 'show active' : '' }}"
+                            id="pills-{{ $menuCategory->id }}" role="tabpanel"
+                            aria-labelledby="pills-{{ $menuCategory->id }}-tab">
+                            <div class="row mx-0">
+                                @foreach ($menuCategory->dish_categories->dishes as $dish)
+                                    <div class="col-md-4 mb-4 dish-card {{ in_array($dish->id, $userFavorites) ? 'favorite' : '' }}"
+                                        data-dish-id="{{ $dish->id }}" data-dish-name="{{ $dish->name_ar }}"
+                                        data-aos="zoom-in">
+                                        <div class="plate">
 
-                                        <div class="text-center pt-4">
-                                            <h5>{{ $dish->name_ar }}</h5>
-                                            <span class="badge bg-warning text-dark">
-                                                <i class="fas fa-star"></i>
-                                                الاعلى تقييم
-                                            </span>
-                                            <div class="d-flex justify-content-between pt-4">
-                                                <button class="btn" data-bs-toggle="modal" data-bs-target="#productModal">
-                                                    + أضف الي العربة
-                                                </button>
-                                                <span>{{ $dish->price }} ج . م</span>
+                                                <figure class="plate-img m-0">
+                                                    <img src="{{ asset($dish->image ?? 'front/AlKout-Resturant/SiteAssets/images/logo-with-white-bg.png') }}"
+                                                        alt="{{ $dish->name_ar }}">
+                                                </figure>
+                                            
+                                            <div class="fav">
+                                                <form action="{{ route('add.favorite') }}" method="POST"
+                                                    class="favorite-form">
+                                                    @csrf
+                                                    <input type="hidden" name="dish_id" value="{{ $dish->id }}">
+                                                    <button type="submit" class="btn">
+                                                        <i
+                                                            class="{{ in_array($dish->id, $userFavorites) ? 'fas' : 'far' }} fa-heart"></i>
+                                                    </button>
+                                                </form>
+                                            </div>
+
+                                            <div class="text-center pt-4">
+                                                <h5>{{ $dish->name_ar }}</h5>
+                                                <span class="badge bg-warning text-dark">
+                                                    <i class="fas fa-star"></i> @lang('header.rateall')
+
+                                                </span>
+                                                <div class="d-flex justify-content-between pt-4">
+                                                    <button class="btn" data-bs-toggle="modal"
+                                                        data-bs-target="#productModal"> @lang('header.addtocart')
+                                                        +
+                                                    </button>
+                                                    <span>{{ $dish->price }} ج . م</span>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            @endforeach
+                                @endforeach
+                            </div>
                         </div>
-                    </div>
-                @endif
-            @endforeach
+                    @endif
+                @endforeach
 
             </div>
 
@@ -129,13 +128,13 @@
                 </div>
                 <div class="cart-content p-4">
                     <!-- <figure class="text-center">
-                                                <img src="SiteAssets/images/cart-remove.svg" alt="" width="125" height="125" />
-                                                <figcaption>
-                                                    <h4>
-                                                        لا توجد منتجات
-                                                    </h4>
-                                                </figcaption>
-                                            </figure> -->
+                                                    <img src="SiteAssets/images/cart-remove.svg" alt="" width="125" height="125" />
+                                                    <figcaption>
+                                                        <h4>
+                                                            لا توجد منتجات
+                                                        </h4>
+                                                    </figcaption>
+                                                </figure> -->
 
                     <div class="sideCart-plate p-4 mb-4">
                         <a href="#">
@@ -195,7 +194,7 @@
                 <div class="cart-footer p-4">
                     <a class="btn w-100 d-flex justify-content-between" href="cart.html">
                         <i class="fas fa-shopping-cart"></i>
-                        <span> أذهب الي العربة</span>
+                        <span> أذهب الي العربة </span>
                         <span> 300 ج . م</span>
                     </a>
                 </div>
